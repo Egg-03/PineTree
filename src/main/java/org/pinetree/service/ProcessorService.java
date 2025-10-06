5fa91f7db2a2a2354818dbe5f9526f6441f45abb
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-10-06 13:53:42 +0530

    refactor(api): introduce managed and unmanaged processor queries
    The `ProcessorService` now provides two methods for retrieving
    processor information: `getProcessors()` and `getProcessorsManaged()`.
    
    `getProcessors()` requires the caller to manage COM initialization
    and uninitialization, offering greater flexibility for scenarios
    involving multiple WMI queries within a single COM session.
    
    `getProcessorsManaged()` handles COM lifecycle internally, providing
    a convenient, self-contained method for simple use cases.
    
    Additionally, the original `getProcessor()` method was renamed to
    `getProcessors()` for improved naming consistency when returning a list.

--- src/main/java/org/pinetree/service/ProcessorService.java
+++ src/main/java/org/pinetree/service/ProcessorService.java
@@ -1,7 +1,6 @@
 package org.pinetree.service;
 
 import com.sun.jna.platform.win32.COM.WbemcliUtil;
-import org.apache.commons.lang3.math.NumberUtils;
 import org.pinetree.constants.enums.ProcessorEnum;
 import org.pinetree.entity.Processor;
 import org.pinetree.util.ComUtil;
@@ -11,54 +10,66 @@ import java.util.List;
 
 public class ProcessorService {
 
-    public List<Processor> getProcessor() {
+    /**
+     * Caller has to manage COM initialization and uninitialization.
+     */
+    public List<Processor> getProcessors() {
 
         List<Processor> processorList= new ArrayList<>();
+
+        // Instantiate Query
+        WbemcliUtil.WmiQuery<ProcessorEnum> query = new WbemcliUtil.WmiQuery<>(
+                "root/cimv2",
+                "Win32_Processor",
+                ProcessorEnum.class
+        );
+
+        // Get Result
+        WbemcliUtil.WmiResult<ProcessorEnum> result = query.execute();
+
+        // Enumerate through the results
+        for(int i=0; i< result.getResultCount(); i++) {
+
+            // Build Processor Object
+            Processor processor = Processor.builder()
+                    .deviceId((String) result.getValue(ProcessorEnum.DeviceID, i))
+                    .name((String) result.getValue(ProcessorEnum.Name, i))
+                    .numberOfCores((Integer) result.getValue(ProcessorEnum.NumberOfCores, i))
+                    .threadCount((Integer) result.getValue(ProcessorEnum.ThreadCount, i))
+                    .numberOfLogicalProcessors((Integer) result.getValue(ProcessorEnum.NumberOfLogicalProcessors, i))
+                    .manufacturer((String) result.getValue(ProcessorEnum.Manufacturer, i))
+                    .addressWidth((Integer) result.getValue(ProcessorEnum.AddressWidth, i))
+                    .l2CacheSize((Integer) result.getValue(ProcessorEnum.L2CacheSize, i))
+                    .l3CacheSize((Integer) result.getValue(ProcessorEnum.L3CacheSize, i))
+                    .maxClockSpeed((Integer) result.getValue(ProcessorEnum.MaxClockSpeed, i))
+                    .extClock((Integer) result.getValue(ProcessorEnum.ExtClock, i))
+                    .socketDesignation((String) result.getValue(ProcessorEnum.SocketDesignation, i))
+                    .version((String) result.getValue(ProcessorEnum.Version, i))
+                    .caption((String) result.getValue(ProcessorEnum.Caption, i))
+                    .family((Integer) result.getValue(ProcessorEnum.Family, i))
+                    .stepping((String) result.getValue(ProcessorEnum.Stepping, i))
+                    .virtualizationFirmwareEnabled((Boolean) result.getValue(ProcessorEnum.VirtualizationFirmwareEnabled, i))
+                    .processorId((String) result.getValue(ProcessorEnum.ProcessorId, i))
+                    .build();
+
+            processorList.add(processor);
+        }
+        return processorList;
+    }
+
+    /**
+     * Caller does not have to manage COM initialization and uninitialization.
+     */
+    public List<Processor> getProcessorsManaged() {
+
         try {
             // Initialize COM
             ComUtil.initialize();
 
-            // Instantiate Query
-            WbemcliUtil.WmiQuery<ProcessorEnum> query = new WbemcliUtil.WmiQuery<>(
-                    "root/cimv2",
-                    "Win32_Processor",
-                    ProcessorEnum.class
-            );
-
-            // Get Result
-            WbemcliUtil.WmiResult<ProcessorEnum> result = query.execute();
-
-            // Enumerate through the results
-            for(int i=0; i< result.getResultCount(); i++) {
-
-                Processor processor = Processor.builder()
-                        .deviceId((String) result.getValue(ProcessorEnum.DeviceID, i))
-                        .name((String) result.getValue(ProcessorEnum.Name, i))
-                        .numberOfCores((Integer) result.getValue(ProcessorEnum.NumberOfCores, i))
-                        .threadCount((Integer) result.getValue(ProcessorEnum.ThreadCount, i))
-                        .numberOfLogicalProcessors((Integer) result.getValue(ProcessorEnum.NumberOfLogicalProcessors, i))
-                        .manufacturer((String) result.getValue(ProcessorEnum.Manufacturer, i))
-                        .addressWidth((Integer) result.getValue(ProcessorEnum.AddressWidth, i))
-                        .l2CacheSize((Integer) result.getValue(ProcessorEnum.L2CacheSize, i))
-                        .l3CacheSize((Integer) result.getValue(ProcessorEnum.L3CacheSize, i))
-                        .maxClockSpeed((Integer) result.getValue(ProcessorEnum.MaxClockSpeed, i))
-                        .extClock((Integer) result.getValue(ProcessorEnum.ExtClock, i))
-                        .socketDesignation((String) result.getValue(ProcessorEnum.SocketDesignation, i))
-                        .version((String) result.getValue(ProcessorEnum.Version, i))
-                        .caption((String) result.getValue(ProcessorEnum.Caption, i))
-                        .family((Integer) result.getValue(ProcessorEnum.Family, i))
-                        .stepping((String) result.getValue(ProcessorEnum.Stepping, i))
-                        .virtualizationFirmwareEnabled((Boolean) result.getValue(ProcessorEnum.VirtualizationFirmwareEnabled, i))
-                        .processorId((String) result.getValue(ProcessorEnum.ProcessorId, i))
-                        .build();
-
-                processorList.add(processor);
-            }
-            return processorList;
+            return getProcessors();
 
         } finally {
             ComUtil.uninitialize();
-
         }
     }
 }
