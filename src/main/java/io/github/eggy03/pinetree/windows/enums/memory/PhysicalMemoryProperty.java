16fd3b2fbec0e2b99e0184192c63c897127065db
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-10-11 21:07:15 +0530

    docs(api): link microsoft docs to PhysicalMemoryProperty

--- src/main/java/io/github/eggy03/pinetree/windows/enums/memory/PhysicalMemoryProperty.java
+++ src/main/java/io/github/eggy03/pinetree/windows/enums/memory/PhysicalMemoryProperty.java
@@ -6,6 +6,7 @@ package io.github.eggy03.pinetree.windows.enums.memory;
  * Each constant represents a property name that can be used to extract
  * specific RAM-related information via a WMI query.
  * @since 1.0
+ * @see <a href="https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-physicalmemory">Win32_PhysicalMemory</a>
  */
 public enum PhysicalMemoryProperty {
 
